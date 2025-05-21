ed3600d5f2093201f6ff1e2be12d60caa5da3f4f
Priya Jha <priyajha.242cs030@nitk.edu.in>
2025-05-21 13:31:37 +0530

    nltk download

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 
+
+
+# Try to download punkt if not already available
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+
 ps = PorterStemmer()
 
 
